4c7439ff363193a8236e864c54564b7af5b5528a
Clement GENAY <clement@gosnat.com>
2022-12-01 18:35:38 +0100

    add new version

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // dépendance api express
 let express = require('express');
+var fs = require("fs");
 
 const app = express();
 
@@ -7,21 +8,13 @@ let bodyParser = require("body-parser");
 
 var PORT = 3000;
 
+var _ = require("underscore");
+
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-app.get('/test/:commune', function(request, response){
-        console.log(request);
-
-    response.render("../Resultat.ejs", {
-    a: 30,
-    b: 86
-});
-  });
-
-// serveur html
+var array=require("array");
 
 
 var server = app.listen(process.env.PORT || 3000, function () {
@@ -30,41 +23,65 @@ var server = app.listen(process.env.PORT || 3000, function () {
   console.log('App listening at http://%s:%s', host, port)
 })
 
+app.get('/getAll', function(request, response) {
+const data = fs.readFileSync('Data/marks.json');
+response.send(JSON.parse(data));
+});
+
+app.get('/get/:ville', function(request, response) {
+const data = fs.readFileSync('Data/marks.json');
+var listeVille = JSON.parse(data);
+var filtered = _.where(listeVille, {nomCommune: request.params.ville});
+response.send(filtered);
+});
 
 
-
-/* app.use(express.json()); */
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//méthode get qui affiche le formulaire
-app.get('/note', function(request, response) {
-  response.sendFile( __dirname  + '/post_note.html');
-});
 
-app.get('/celia', function(request, response) {
+
+app.get('/', function(request, response) {
   response.sendFile( __dirname  + '/index.html');
 });
 
 
 
-app.get('/result', function(request, response) {
-  let commune = request.body.nomCommune; //string type
-
-  /* fonction Théo */
-
-  console.log("commune = " + commune);
-});
-
 //le formulaire pointe en sortie vers la méthode post ci-après, permettant de récupérer la note saisie
 app.post('/post', function(request, response) {
   const body = request.body;
-  let rg1 = body.rg1; //string type
-  let commune = body.nomCommune2; //string type
+  let note = body.note; //string type
+  let commune = body.nomCommune; //string type
+  let codeCP = body.codeCP; // string type
 
-
-  /* fonction Théo */
-  response.redirect(`/test/`+commune);
-
+   response.redirect('/Resultats/'+commune+'/'+codeCP);
 
 });
+
+
+
+app.get('/Resultats/:commune/:CP', function(request, response){
+
+const data = fs.readFileSync('Data/marks.json');
+var listeVille = JSON.parse(data);
+//console.log(listeVille);
+var filtered = _.where(listeVille, {nomCommune: request.params.commune});
+
+
+var code=filtered[0]['codeCommune'];
+var nom=filtered[0]['nomCommune'];
+var int=filtered[0]['scoreInternet'];
+var gaz=filtered[0]['scoreGaz'];
+var ed=filtered[0]['scoreBB'];
+var bio=filtered[0]['scoreBio'];
+
+    response.render("../Resultat.ejs", {
+    a: code,
+    b: nom,
+    c: int,
+    d: gaz,
+    e: ed,
+    f:bio
+    
+});
+  });
